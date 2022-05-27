8ac46b0a8c9fed7ecbc958fe57c1afd4c77e0921
alexcypriiz <alexey.podlubny@yandex.ru>
2022-05-27 20:52:26 +0400

    feat: Add default order entry

--- src/main/java/ru/aisa/carwash/service/OrderEntityService.java
+++ src/main/java/ru/aisa/carwash/service/OrderEntityService.java
@@ -23,23 +23,44 @@ public class OrderEntityService {
     }
 
     public boolean saveOrder(OrderEntity orderEntity, Client client) {
+        //    Затрачиваемое время с учетом всех услг
+        LocalDateTime startTime;
+        LocalDateTime endTime;
+        Integer fullTime = orderEntity.getWashOptions().stream().mapToInt(x -> x.getTime()).sum();
+        String orderEntityFromDB;
+
+//        если клиент выбрал время
+        if (orderEntity.getStartTime() != null) {
 //        нахождение конца интервала времени
-        LocalDateTime endTime = orderEntity.getStartTime().plusMinutes(orderEntity.getWashOptions().stream().mapToInt(x -> x.getTime()).sum());
+            startTime = orderEntity.getStartTime();
+            endTime = orderEntity.getStartTime().plusMinutes(fullTime);
 
 //        проверка на пересечение по времени
-        String orderEntityFromDB = orderEntityRepository.findByStartTimeAndEndTime(orderEntity.getStartTime(), endTime);
-
-        if (orderEntityFromDB != null) {
-            return false;
+            orderEntityFromDB = orderEntityRepository.findByStartTimeAndEndTime(orderEntity.getStartTime(), endTime);
+            if (orderEntityFromDB != null) {
+                return false;
+            }
+        } else {
+            String checkOrderEntityFromDB = orderEntityRepository.findByStartTimeAndEndTime(LocalDateTime.now(), LocalDateTime.now().plusMinutes(fullTime));
+//            если сейчас не свободно время
+            if (checkOrderEntityFromDB != null) {
+                LocalDateTime freeStartTime = orderEntityRepository.findNearestByStartTime(fullTime);
+                startTime = freeStartTime;
+                endTime = freeStartTime.plusMinutes(fullTime);
+            } else {
+                startTime = LocalDateTime.now();
+                endTime = LocalDateTime.now().plusMinutes(fullTime);
+//                orderEntityFromDB = checkOrderEntityFromDB;
+            }
         }
 
 //        бронирование времени с учетом всех данных
-        orderEntityRepository.insertOrder(orderEntity.getStartTime(), endTime, client.getEmail(),
-                client.getUsername(), orderEntity.getWashOptions().stream().mapToInt(x -> x.getCost()).sum(),
-                client.getBrandCar(), client.getNumberCar());
+        orderEntityRepository.insertOrder(startTime, endTime, client.getEmail().trim(),
+                client.getUsername().trim(), orderEntity.getWashOptions().stream().mapToInt(x -> x.getCost()).sum(),
+                client.getBrandCar().trim(), client.getNumberCar().trim());
 
 //        нахождение ид записи для связывания с услугами
-        Long idOrder = orderEntityRepository.findIdByStartTimeAndEndTime(orderEntity.getStartTime(), endTime);
+        Long idOrder = orderEntityRepository.findIdByStartTimeAndEndTime(startTime, endTime);
 
 //        связывание записи с услугами, в дальнейшем может пригодиться для отображения услуг, которые выполняют определенные рабочие
         orderEntity.getWashOptions().stream().forEach(x -> orderEntityRepository.bindOrderAndWashOption(idOrder, x.getId()));
